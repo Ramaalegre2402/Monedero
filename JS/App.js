c452e01a2a687bc86d857a9f41506519f9dce6eb
Ramaalegre2402 <rama.alegre@gmail.com>
2023-01-07 16:58:43 -0400

    commit2 html y css1

--- JS/App.js
+++ JS/App.js
@@ -1,1 +1,10 @@
-console.log(`holas`)
+let saldo=document.getElementById('saldo')
+saldo.innerText= 5000
+
+let totalIngreso=document.getElementById(`totalIngreso`)
+totalIngreso.innerText=10000
+
+let totalGasto=document.getElementById(`totalGasto`)
+totalGasto.innerText=5000
+
+
